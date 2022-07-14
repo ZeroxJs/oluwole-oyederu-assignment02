a19014d3c34e2a0a57dd92ccc9b064cebbbcf84c
Oluwole Oyederu <105388682+ZeroxJs@users.noreply.github.com>
2022-07-14 18:03:27 +0100

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -16,7 +16,7 @@ const assignment = {};
 function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
